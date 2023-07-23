cd0de05b6382ca4c91ad0e3b9705154bd4541564
RealSeek <1536266519@qq.com>
2023-07-23 17:44:36 +0800

    perf: Optimized unused files in the config.

--- src/main/java/net/flandre923/fabrickookchat/KookServerMod.java
+++ src/main/java/net/flandre923/fabrickookchat/KookServerMod.java
@@ -14,19 +14,13 @@ import snw.jkook.config.file.YamlConfiguration;
 import snw.kookbc.impl.CoreImpl;
 import snw.kookbc.impl.KBCClient;
 
-import java.io.File;
-import java.io.FileOutputStream;
-import java.io.IOException;
-import java.io.InputStream;
+import java.io.*;
+import java.nio.charset.StandardCharsets;
 
 // shift + f6 更改这个类的名字
 public class KookServerMod implements DedicatedServerModInitializer {
 	public static final String MOD_ID = "fabrickookchat";
     public static final Logger LOGGER = LoggerFactory.getLogger(MOD_ID);
-	private static final File kbcSetting = new File(".", "config/McToKook/kbc.yml");
-	private static final File kbcPluginFolder = new File(".","config/McToKookPlugin");
-	private static final File configFolder = new File(".", "config/McToKook");
-
 	static KBCClient kbcClient = null;
 	public static Config config = ConfigFile.DEFAULT_CONFIG;
 	public static ConfigFile configFile = new ConfigFile("kook.json");
@@ -52,21 +46,10 @@ public class KookServerMod implements DedicatedServerModInitializer {
 				writeConfig();
 			}
 
-			if (!configFolder.exists()) {
-				configFolder.mkdir();
-			}
-			if(!kbcPluginFolder.exists()){
-				kbcPluginFolder.mkdir();
-			}
-
 			//KookBC保存基础配置文件
-			saveKBCConfig();
 			YamlConfiguration config = new YamlConfiguration();
-			try {
-				config.load(kbcSetting);
-			} catch (IOException | InvalidConfigurationException e) {
-				e.printStackTrace();
-			}
+			saveKBCConfig(config);
+
 			CoreImpl core = new CoreImpl();
 			JKook.setCore(core);
 
@@ -86,7 +69,8 @@ public class KookServerMod implements DedicatedServerModInitializer {
 				}
 			}
 			LOGGER.info("info token :" + KookServerMod.config.bot_token + " -- channel id" + KookServerMod.config.channel_ID);
-			kbcClient = new KBCClient(core, config, new File(".","config/McToKook/"), bot_token);
+			// pluginsFolder 传入 null,不会尝试从任何地方加载插件（或者后续需要使用 KookBC 的 plugin?）
+			kbcClient = new KBCClient(core, config, null, bot_token);
 
 			kbcClient.start();
 			LOGGER.info("机器人启动！");
@@ -100,26 +84,34 @@ public class KookServerMod implements DedicatedServerModInitializer {
 	}
 
 	/**
-	 *
+	 * 传入一个 config,使用 stream 读取
+	 * @param config
+	 * @author RealSeek
 	 */
-	private static void saveKBCConfig() {
+	private static void saveKBCConfig(YamlConfiguration config) {
 		try (final InputStream stream = KookServerMod.class.getResourceAsStream("/kbc.yml")) {
 			if (stream == null) {
 				throw new Error("Unable to find kbc.yml");
 			}
-			if (kbcSetting.exists()) {
-				return;
-			}
-			//noinspection ResultOfMethodCallIgnored
-			kbcSetting.createNewFile();
 
-			try (final FileOutputStream out = new FileOutputStream(kbcSetting)) {
-				int index;
-				byte[] bytes = new byte[1024];
-				while ((index = stream.read(bytes)) != -1) {
-					out.write(bytes, 0, index);
+			StringBuilder yamlContentBuilder = new StringBuilder();
+			try (BufferedReader reader = new BufferedReader(new InputStreamReader(stream, StandardCharsets.UTF_8))) {
+				String line;
+				while ((line = reader.readLine()) != null) {
+					yamlContentBuilder.append(line).append(System.lineSeparator());
 				}
+			} catch (IOException e) {
+				e.printStackTrace();
 			}
+
+			String yamlContent = yamlContentBuilder.toString();
+
+			try {
+				config.loadFromString(yamlContent);
+			} catch (InvalidConfigurationException e) {
+				e.printStackTrace();
+			}
+
 		} catch (IOException e) {
 			e.printStackTrace();
 		}
